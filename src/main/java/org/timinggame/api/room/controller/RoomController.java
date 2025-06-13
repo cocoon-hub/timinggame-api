bcfbb56e5779630ac8cb5cf68afb734d1b01fb1e
simhani1 <simhani1@gmail.com>
2025-06-14 00:49:00 +0900

    test: 게임 시작시 음수가 넘어오면 실패한다

--- src/main/java/org/timinggame/api/room/controller/RoomController.java
+++ src/main/java/org/timinggame/api/room/controller/RoomController.java
@@ -1,6 +1,5 @@
 package org.timinggame.api.room.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +9,9 @@ import org.timinggame.api.room.controller.response.CreateRoomRes;
 import org.timinggame.api.room.domain.Room;
 import org.timinggame.api.room.service.RoomService;
 
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+
 @RequestMapping("/v1/room")
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +20,7 @@ public class RoomController {
     private final RoomService roomService;
 
     @PostMapping("/start/{roomId}")
-    public ResponseEntity<CreateRoomRes> startGame(@PathVariable("roomId") Long roomId) {
+    public ResponseEntity<CreateRoomRes> startGame(@PathVariable("roomId") @Positive Long roomId) {
         Room room = roomService.startGame(roomId);
         return ResponseEntity.ok(CreateRoomRes.builder().roomId(room.getRoomId()).build());
     }
--- src/test/java/org/timinggame/api/room/controller/RoomControllerTest.java
+++ src/test/java/org/timinggame/api/room/controller/RoomControllerTest.java
@@ -7,7 +7,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.timinggame.api.RoomControllerUnitTest;
 import org.timinggame.api.fixture.RoomFixture;
@@ -33,4 +35,18 @@ class RoomControllerTest extends RoomControllerUnitTest {
 			.andDo(print());
 	}
 
+	@RepeatedTest(5)
+	void 방_ID가_0이하면_예외가_발생한다() throws Exception {
+		// GIVEN
+		final Long roomId = -(new Random().nextLong(Long.MAX_VALUE));
+		final Room room = RoomFixture.inProgressRoom(LocalDateTime.now(), null);
+
+		// WHEN
+		when(roomService.startGame(anyLong())).thenReturn(room);
+
+		// THEN
+		mockMvc.perform(post(START_GAME_URL, roomId))
+			.andExpect(status().isBadRequest())
+			.andDo(print());
+	}
 }
