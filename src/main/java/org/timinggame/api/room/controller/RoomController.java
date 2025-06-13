45b5aa81af3b29090080c7156c4c24a21090771f
simhani1 <simhani1@gmail.com>
2025-06-13 21:31:20 +0900

    feat: controller를 구현한다

---
+++ src/main/java/org/timinggame/api/room/controller/RoomController.java
@@ -0,0 +1,28 @@
+package org.timinggame.api.room.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.timinggame.api.room.controller.response.CreateRoomRes;
+import org.timinggame.api.room.domain.Room;
+import org.timinggame.api.room.service.RoomService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/v1/room")
+@RestController
+@RequiredArgsConstructor
+public class RoomController {
+
+	private final RoomService roomService;
+
+	@PostMapping("/start/{roomId}")
+	public ResponseEntity<CreateRoomRes> startGame(@PathVariable("roomId") Long roomId) {
+		Room room = roomService.startGame(roomId);
+		return ResponseEntity.ok(CreateRoomRes.builder()
+			.roomId(room.getRoomId())
+			.build());
+	}
+}
---
+++ src/main/java/org/timinggame/api/room/controller/response/CreateRoomRes.java
@@ -0,0 +1,16 @@
+package org.timinggame.api.room.controller.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateRoomRes {
+
+	private Long roomId;
+}
