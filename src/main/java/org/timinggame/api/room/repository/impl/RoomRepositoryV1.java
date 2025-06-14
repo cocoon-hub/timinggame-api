8f23985a67006b8cf47c73c8e71286c1ba5384c0
simhani1 <simhani1@gmail.com>
2025-06-15 00:47:20 +0900

    style: sql 공백을 제거한다

--- src/main/java/org/timinggame/api/room/repository/impl/RoomRepositoryV1.java
+++ src/main/java/org/timinggame/api/room/repository/impl/RoomRepositoryV1.java
@@ -18,15 +18,15 @@ public class RoomRepositoryV1 implements RoomRepository {
     public Optional<Room> findById(final Long roomId) {
         String sql =
                 """
-			SELECT game_room_id as room_id,
-						       pin_code,
-						       status,
-						       loser_id,
-						       started_at,
-						       finished_at,
-						       created_at
-						FROM game_room WHERE game_room_id = ?
-			""";
+				SELECT game_room_id as room_id,
+										       pin_code,
+										       status,
+										       loser_id,
+										       started_at,
+										       finished_at,
+										       created_at
+				FROM game_room WHERE game_room_id = ?;
+				""";
         return Optional.ofNullable(
                 jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Room.class), roomId));
     }
