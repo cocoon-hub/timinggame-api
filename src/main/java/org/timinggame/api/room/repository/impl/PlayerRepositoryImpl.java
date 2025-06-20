e3c43ebb2267cd3dbd4e29b518f4d467075eb427
gnsdp99 <rladpgns99@gmail.com>
2025-06-20 20:31:05 +0900

    feat: 게임 플레이어 관련 repository 인터페이스와 구현체를 추가한다

---
+++ src/main/java/org/timinggame/api/room/repository/impl/PlayerRepositoryImpl.java
@@ -0,0 +1,14 @@
+package org.timinggame.api.room.repository.impl;
+
+import org.springframework.stereotype.Repository;
+import org.timinggame.api.room.repository.PlayerRepository;
+
+@Repository
+public class PlayerRepositoryImpl implements PlayerRepository {
+
+    @Override
+    public Long getPlayerCount(final Long roomId) {
+        // TODO: Redis 연결
+        return 0L;
+    }
+}
