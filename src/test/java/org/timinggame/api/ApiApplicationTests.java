3bfbe84d57580bc15dc5b72f6f58036fe19d7b36
simhani1 <simhani1@gmail.com>
2025-06-22 23:29:14 +0900

    feat: mongodb 자동설정을 제외한다

--- src/test/java/org/timinggame/api/ApiApplicationTests.java
+++ src/test/java/org/timinggame/api/ApiApplicationTests.java
@@ -2,8 +2,12 @@ package org.timinggame.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 class ApiApplicationTests {
 
 	@Test
