8e624276caeca971c352d2a8435344e50e93f472
simhani1 <simhani1@gmail.com>
2025-06-22 19:38:21 +0900

    feat: 예외 핸들러를 추가한다

--- src/main/java/org/timinggame/api/room/controller/RoomController.java
+++ src/main/java/org/timinggame/api/room/controller/RoomController.java
@@ -1,6 +1,7 @@
 package org.timinggame.api.room.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.timinggame.api.room.service.RoomService;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 
+@Validated
 @RequestMapping("/v1/room")
 @RestController
 @RequiredArgsConstructor
