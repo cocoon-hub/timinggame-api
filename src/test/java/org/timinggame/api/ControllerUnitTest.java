c3a49168af902510e13e80f255ade6c616adcb8a
Jonghan Sim <simhani1@gmail.com>
2025-06-24 00:36:45 +0900

    feat: mongodb 의존성을 추가한다 (#22)
    <!-- 제목 양식을 지켜주세요! ex. feat: 내용내용내용 -->
    ### Issue Number
    - close #20 
    
    ### Summary 
    <!-- 구현 기능 설명해주세요 -->
    - 통합 테스트는 없기 때문에 자동설정을 제외시켰습니다.
    - 이후에는 테스트컨테이너를 사용해서 테스트 환경에서 몽고DB를 사용할 수 있도록 설정해야 합니다.
    
    ### Other
    <!-- 기타 -->

--- src/test/java/org/timinggame/api/ControllerUnitTest.java
+++ src/test/java/org/timinggame/api/ControllerUnitTest.java
@@ -1,9 +1,10 @@
 package org.timinggame.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest
 public abstract class ControllerUnitTest {
